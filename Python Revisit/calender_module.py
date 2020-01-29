99cce589e397898f3f40738492eb482ab77bfa2d
uppu.phanisai@gmail.com <uppu.phanisai@gmail.com>
2020-01-29 18:40:41 +0530

    python revisit

--- Python Revisit/calender_module.py
+++ Python Revisit/calender_module.py
@@ -1,1 +1,22 @@
 import calendar
+
+print(calendar.weekheader(3))
+print()
+
+print(calendar.firstweekday())
+print()
+# Month in calendar form
+print(calendar.month(2000, 2))
+print()
+# Month in Matrix form.
+print(calendar.monthcalendar(2000, 2))
+print()
+
+# Entire Year in Calander view.
+print(calendar.calendar(2000))
+
+print(calendar.weekday(2000, 2, 22))
+print(calendar.isleap(2000))
+
+how_many_leap_days = calendar.leapdays(2000, 2020)
+print(how_many_leap_days)
