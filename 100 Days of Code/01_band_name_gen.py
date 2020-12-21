70f944d860fd339e0c0dce5875922791232d23b6
phanisai22 <uppu.phanisai@gmail.com>
2020-12-21 14:56:37 +0530

    day 1

---
+++ 100 Days of Code/01_band_name_gen.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator.")
+city_n = input("What's name of the city you grew up in?\n")
+pet_n = input("What's name of your pet?\n")
+
+print("Your band name is " + city_n + " "+pet_n)
